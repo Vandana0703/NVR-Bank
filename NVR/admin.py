5c1fa29fce3f41756331e16df09b07ba23dc0ded
Vandana0703 <86510062+Vandana0703@users.noreply.github.com>
2021-06-26 12:14:39 +0530

    Add files via upload

---
+++ NVR/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import customerdetail,transectiondetail
+
+# Register your models here.
+admin.site.register(customerdetail)
+admin.site.register(transectiondetail)
